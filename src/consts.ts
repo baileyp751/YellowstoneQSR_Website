f0b95acfb8a4e77b82cdef8ddc5fea4f29151cae
baileyp751 <138946533+baileyp751@users.noreply.github.com>
2025-05-26 21:27:08 -0600

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-// Place any global data in this file.
+l// Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Yellowstone QSR";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Yellowstone QSR";
